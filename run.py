84438434921f25e3d4e4c144869da6439b8f08f7
Lukas2020-code <>
2023-11-02 21:53:01 +0000

    Create user input function and store the values in variables

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# function to get user input
+def user_input():
+    print("Please provide some information which helps calculate your BMI")
+
+    while True:
+        try:
+            name = input("Please type your name:\n")
+            print(name)
+
+            email = input("Please write your email:\n")
+            print(email)
+
+            gender = input("Please type your gender (F/M):\n")
+            print(gender)
+
+            weight = float(input("Please type your weight in kg:\n"))
+            print(weight)
+
+            height = float(input("Please provide your height in cm:\n"))
+            print(height)
+
+            dob = input("Enter you date of birth in format dd/mm/yyyy:\n")
+            print(dob)
+
+            break
+
+        except ValueError as e:
+            print(f'Oops, something wnet wrong! Validation error: {e}. Try again...')
+
+    return name, email, gender, weight, height, dob        
