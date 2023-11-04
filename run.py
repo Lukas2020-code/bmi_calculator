7c0cc1cb376c3915d59ed36860b840e58659948e
Lukas2020-code <>
2023-11-04 00:41:22 +0000

    Connect to  API and establish access to google sheets to store users informations

--- run.py
+++ run.py
@@ -2,6 +2,21 @@ import re
 from email_validator import validate_email, EmailNotValidError
 from datetime import datetime
 from PIL import Image
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('calculate_user_bmi')
+
+users = SHEET.worksheet('users')
 
 
 def has_numbers(name):
